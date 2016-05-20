6107cfd3d57cecdf18b1b7ab0ee0cdf45683d4ca
ljq <ljq@zczczy.com>
2016-05-20 21:45:33 +0800

    购物车

--- app/src/main/java/com/zczczy/leo/microwarehouse/fragments/CartFragment.java
+++ app/src/main/java/com/zczczy/leo/microwarehouse/fragments/CartFragment.java
@@ -1,12 +1,20 @@
 package com.zczczy.leo.microwarehouse.fragments;
 
 import com.zczczy.leo.microwarehouse.R;
+import com.zczczy.leo.microwarehouse.viewgroup.MyTitleBar;
 
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by Leo on 2016/5/20.
  */
-@EFragment(R.layout.fragment_home)
+@EFragment(R.layout.fragment_cart)
 public class CartFragment extends BaseFragment {
+
+    @ViewById
+    MyTitleBar myTitleBar;
+
+
+
 }
--- app/src/main/java/com/zczczy/leo/microwarehouse/fragments/HomeFragment.java
+++ app/src/main/java/com/zczczy/leo/microwarehouse/fragments/HomeFragment.java
@@ -1,12 +1,50 @@
 package com.zczczy.leo.microwarehouse.fragments;
 
-import com.zczczy.leo.microwarehouse.R;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.daimajia.slider.library.SliderLayout;
+import com.zczczy.leo.microwarehouse.R;
+import com.zczczy.leo.microwarehouse.viewgroup.MyTitleBar;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.ViewsById;
+
+import java.util.List;
 
 /**
  * Created by Leo on 2016/5/20.
  */
 @EFragment(R.layout.fragment_home)
 public class HomeFragment extends BaseFragment {
+
+    @ViewById
+    MyTitleBar myTitleBar;
+
+    @ViewById
+    SliderLayout homeSlider;
+
+    @ViewById
+    TextView txt_one, txt_two, txt_three, txt_four;
+
+    @ViewsById(value = {R.id.ad_one, R.id.ad_two, R.id.ad_three, R.id.ad_four, R.id.ad_five, R.id.ad_six, R.id.ad_seven, R.id.ad_eight, R.id.ad_nine})
+    List<ImageView> imageViewList;
+
+
+
+    @AfterViews
+    void afterView() {
+
+    }
+
+
+    @Click(value = {R.id.ad_one, R.id.ad_two, R.id.ad_three, R.id.ad_four, R.id.ad_five, R.id.ad_six, R.id.ad_seven, R.id.ad_eight, R.id.ad_nine})
+    void imageViewList(ImageView imageView) {
+
+    }
+
 }
