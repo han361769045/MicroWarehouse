df90f3c139131477d8256df1aaf8541607b443c3
BY <457660696@qq.com>
2016-05-21 15:48:34 +0800

    增加图片

--- app/src/main/java/com/zczczy/leo/microwarehouse/fragments/MineFragment.java
+++ app/src/main/java/com/zczczy/leo/microwarehouse/fragments/MineFragment.java
@@ -1,20 +1,11 @@
 package com.zczczy.leo.microwarehouse.fragments;
 
-import android.graphics.drawable.Drawable;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.TextView;
+
 
 import com.zczczy.leo.microwarehouse.R;
-import com.zczczy.leo.microwarehouse.tools.AndroidTool;
-import com.zczczy.leo.microwarehouse.viewgroup.MyTitleBar;
 
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.ViewById;
+
 
 /**
  * Created by Leo on 2016/5/20.
