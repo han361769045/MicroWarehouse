82293b422654d774b7db8b4ee75792aebe1b2619
BY <457660696@qq.com>
2016-05-21 13:06:33 +0800

    登陆页面完成

---
+++ app/src/main/java/com/zczczy/leo/microwarehouse/activities/AccountManagementActivity.java
@@ -0,0 +1,13 @@
+package com.zczczy.leo.microwarehouse.activities;
+
+import com.zczczy.leo.microwarehouse.R;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by zczczy on 2016/5/21.
+ *  账户管理
+ */
+@EActivity(R.layout.activity_account_managerment)
+public class AccountManagementActivity extends  BaseActivity {
+}
---
+++ app/src/main/java/com/zczczy/leo/microwarehouse/activities/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.zczczy.leo.microwarehouse.activities;
+
+import com.zczczy.leo.microwarehouse.R;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by zczczy on 2016/5/21.
+ */
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends BaseActivity {
+}
