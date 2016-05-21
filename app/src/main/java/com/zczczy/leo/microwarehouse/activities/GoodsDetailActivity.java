2faec93905d84bf586819408e20e5585bb3a06c6
BY <457660696@qq.com>
2016-05-21 14:07:49 +0800

    商品详情

--- app/src/main/java/com/zczczy/leo/microwarehouse/activities/GoodsDetailActivity.java
+++ app/src/main/java/com/zczczy/leo/microwarehouse/activities/GoodsDetailActivity.java
@@ -2,15 +2,11 @@ package com.zczczy.leo.microwarehouse.activities;
 
 import com.zczczy.leo.microwarehouse.R;
 
-import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 
 /**
  * Created by zczczy on 2016/5/21.
  */
-@EActivity(R.layout.activity_login)
-public class LoginActivity extends BaseActivity {
-
-
-
+@EActivity(R.layout.activity_goods_detail)
+public class GoodsDetailActivity extends  BaseActivity {
 }
--- app/src/main/java/com/zczczy/leo/microwarehouse/fragments/MineFragment.java
+++ app/src/main/java/com/zczczy/leo/microwarehouse/fragments/MineFragment.java
@@ -1,12 +1,29 @@
 package com.zczczy.leo.microwarehouse.fragments;
 
-import com.zczczy.leo.microwarehouse.R;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import com.zczczy.leo.microwarehouse.R;
+import com.zczczy.leo.microwarehouse.tools.AndroidTool;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by Leo on 2016/5/20.
  */
-@EFragment(R.layout.activity_login)
+@EFragment(R.layout.activity_goods_detail)
 public class MineFragment extends BaseFragment {
+    @ViewById
+    WebView web_view;
+
+
+    @AfterViews
+    void  afterView(){
+        web_view.loadUrl("http://www.zczczy.com");
+
+    }
 }
